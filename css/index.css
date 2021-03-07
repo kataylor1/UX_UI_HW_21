1bffe3f7531b0516d82d0f9bb60f40667fde76ef
kataylor1 <71146830+kataylor1@users.noreply.github.com>
2021-03-06 23:39:13 -0500

    First draft of Hobby Shop
    Homework 21
    Bootstrap5 Hobby Shop website with Google Analytics tags, basic layout and a few placeholder images.  Working navigation.

---
+++ css/index.css
@@ -0,0 +1,49 @@
+.container {
+    max-width: 960px;
+  }
+  
+  .pricing-header {
+    max-width: 700px;
+  }
+
+.head {
+    padding-top: 20px;
+    padding-bottom: 20px;
+}
+
+.shopName {
+    text-align: left;
+    color: coral;
+}  
+
+h4 {
+    color:coral;
+    font-weight: bold;
+}
+
+.card-puzzle {
+    background-image: url('../images/debby-hudson-tDaeJbTRwxQ-unsplash.jpg');
+    background-repeat: no-repeat;
+    background-position: bottom center;
+    background-size: cover;
+    /* color: coral; test colour */
+    min-height: 350px;
+}
+
+.card-family {
+    background-image: url('../images/national-cancer-institute-VJVsEnR_vNE-unsplash.jpg');
+    background-repeat: no-repeat;
+    background-position: bottom center;
+    background-size: cover;
+    /* color: coral; test colour */
+    min-height: 350px;
+}
+
+.card-tricks {
+    background-image: url('../images/jose-aljovin-ZR_YT4Kp5Ik-unsplash.jpg');
+    background-repeat: no-repeat;
+    background-position: bottom center;
+    background-size: cover;
+    /* color: coral; test colour */
+    min-height: 350px;
+}
